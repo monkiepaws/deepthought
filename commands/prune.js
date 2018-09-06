a11912adb2af6ddee055be5dab26b22584faa815
Richard Sims <9633324@student.swin.edu.au>
2018-09-06 20:19:34 +1000

    added base files after completing discord.js tutorial

---
+++ commands/prune.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "prune",
+    description: "Prune messages!",
+    args: true,
+    usage: "[amount]",
+    execution(message, args) {
+        const amount = args[0];
+
+        if (isNaN(amount)) {
+            return message.reply("that's not quite a number, is it?");
+
+        } else if (amount < 2 || amount > 100) {
+            return message.reply("you can prune the last 2 to 100 messages.");
+        }
+
+        message.channel.bulkDelete(amount);
+    }
+};
--- index.js
+++ index.js
@@ -1,9 +1,84 @@
+const fs = require("fs");
 const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+
 const client = new Discord.Client();
-const auth = require("./auth.json");
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
 
 client.on("ready", () => {
     console.log("Ready!");
 });
 
-client.login(auth.token);
+client.login(token);
+
+client.on("message", message => {
+    if (message.content.startsWith(prefix) === false || message.author.bot === true) {
+        return;
+    }
+
+    // Split message of all spaces
+    const args = message.content.slice(prefix.length).split(/ +/);
+    // Remove first word from the message as the command
+    const commandName = args.shift().toLowerCase();
+
+    // Set the command name provided by the user, OR by using an alias
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    // If not a valid command, return
+    if (!command) return;
+
+    // If the command doesn't have the right number of arguments...
+    if (command.args && !args.length) {
+        let reply = `Whoah, there! You need to provide some arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage is: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    // Set cooldowns
+    if (cooldowns.has(command.name) === false) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    // Add the user to the cooldown list if not already there, and set a timer to delete them
+    if (timestamps.has(message.author.id) === false) {
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    // Otherwise, check if they meet the criteria (for that command) for activating a cooldown timer
+    } else {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+    
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    }
+
+    // Execute the command
+    try {
+        command.execute(message, args);
+    }
+    catch (error) {
+        console.error(error);
+        message.reply("oh no! Something happened. It wasn't good ~~!");
+    }
+});
