c6fb70348a1cdc1039965055ebe0fbaa2b925054
Richard Sims <9633324@student.swin.edu.au>
2018-09-07 14:27:46 +1000

    added showList(); Deep Thought now posts game waiting lists

--- commands/lfg.js
+++ commands/lfg.js
@@ -10,30 +10,26 @@ const description = "Let people know which games you are available for!\n" +
                     "mhw        Monster Hunter World\n" +
                     "tekken     ...Tekken 7\n"
                     "mvci       Marvel vs Capcom: Infinite\n";
+const validPlatforms = ["pc", "ps4"];
+const defaultPlatform = "pc";
 
-let lfgCollection = [];
+const lfgList = [];
 
 module.exports = {
     name: "lfg",
     usage: "[name of game] [hours] [optional: platform]",
-    aliases: ["games", "waiting"],
+    aliases: ["game", "games", "waiting", "available"],
     description: description,
     execute(message, args) {
+        // !lfg
+        // Without arguments, we show the current list of users waiting for games
         if (args.length === 0) {
-            for (let i = 0; i < lfgCollection.length; i++) {
-                if (lfgCollection[i].hasOwnProperty("endTime")) {
-                    if (Date.now() >= lfgCollection[i].endTime) {
-                        lfgCollection.splice(i, 1);
-                        i--;
-                    }
-                }
-            }
-            console.log(lfgCollection);
+            showList(message);
             return;
         }
 
         const game = args[0];
-        const platform = args[2] || "pc";
+        const platform = args[2] || defaultPlatform;
 
         const availableTime = args[1] * 1000;
         const availableUntil = Date.now() + availableTime; 
@@ -47,7 +43,45 @@ module.exports = {
         }
 
 
-        lfgCollection.push(newLFG);
+        lfgList.push(newLFG);
         return;
     }
 };
+
+const showList = message => {
+    const removeExpiredLFG = i => {
+        if (lfgList[i].hasOwnProperty("endTime")) {
+            if (Date.now() >= lfgList[i].endTime) {
+                lfgList.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    };
+
+    const createListText = i => {
+        const username = lfgList[i].username;
+        const game = lfgList[i].game;
+        const platform = lfgList[i].platform;
+        const endTime = Math.floor((lfgList[i].endTime - Date.now()) / 1000);
+
+        const text = `**${i + 1}. ${username}:** waiting for **${game}** on **${platform}** for ${endTime} more seconds.\n`;
+
+        return text;
+    };
+
+    let listMessage = "**__Games Available:__**\n";
+    
+    for (let i = 0; i < lfgList.length; i++) {
+        if (removeExpiredLFG(i) === true) {
+            i--;
+        } else {
+            listMessage += createListText(i);
+        }       
+    }
+    console.log(lfgList);
+
+    message.channel.send(listMessage);
+
+    return;
+}
