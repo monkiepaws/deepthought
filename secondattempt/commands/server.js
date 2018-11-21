566c701a45c8801c6e24c049f9a0594082178520
Richard Sims <9633324@student.swin.edu.au>
2018-11-21 13:28:06 +1100

    removed unused preliminary tests for pre-release

--- secondattempt/commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "server",
-    description: "Server",
-    execute(message, args) {
-        message.channel.send(`This server's name is: ${message.guild.name}`);
-    }
-};
