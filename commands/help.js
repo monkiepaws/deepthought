81905179aee13884ebc54d0362aa02d13f6c2d16
Richard Sims <9633324@student.swin.edu.au>
2018-11-30 08:55:22 +1100

    issue #2 - testing sending to DM before testing line 43

--- commands/help.js
+++ commands/help.js
@@ -38,10 +38,10 @@ module.exports = {
 
         if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
         if (command.description) data.push(`**Description:** ${command.description}`);
-        // if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
         //
         // data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
-        return message.channel.send(data, { split: true }).catch(error => console.error(error));
+        return message.author.send(data, { split: true }).catch(error => console.error(error));
     } 
 };
