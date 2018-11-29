27fa227b5d39dcb65ba768c7c9569c97fb6d37cc
Richard Sims <9633324@student.swin.edu.au>
2018-11-30 08:42:36 +1100

    testing if issue-2 caused by lines 39-43 of help.js

--- commands/help.js
+++ commands/help.js
@@ -36,11 +36,11 @@ module.exports = {
 
         data.push(`**Name:** ${command.name}`);
 
-        // if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        // if (command.description) data.push(`**Description:** ${command.description}`);
-        // if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-        //
-        // data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) data.push(`**Description:** ${command.description}`);
+        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
         return message.channel.send(data, { split: true }).catch(error => console.error(error));
     } 
