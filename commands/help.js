af5622978163bd0c7a18cd987b334fd74721d732
Richard Sims <9633324@student.swin.edu.au>
2018-11-30 08:51:02 +1100

    issue #2 - testing line 39 of help.js

--- commands/help.js
+++ commands/help.js
@@ -37,10 +37,10 @@ module.exports = {
         data.push(`**Name:** ${command.name}`);
 
         if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+        // if (command.description) data.push(`**Description:** ${command.description}`);
+        // if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        //
+        // data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
         return message.channel.send(data, { split: true }).catch(error => console.error(error));
     } 
