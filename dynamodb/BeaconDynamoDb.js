5723486dbdc97ddd8e65531d8892ca6ff240dbdb
Richard Sims <9633324@student.swin.edu.au>
2020-05-02 16:46:19 +1000

    Added test getAllAvailableBeacons function

---
+++ dynamodb/BeaconDynamoDb.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "ap-southeast-2",
+    endpoint: "http://localhost:8000"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = "WP_Beacons";
+
+module.exports = class BeaconDynamoDb {
+    constructor() {
+    }
+
+    async getAvailableBeacons() {
+        const dateTimeNow = Date.now()
+        const params = {
+            TableName: table,
+            IndexName: "AllAvailableBeacons",
+            KeyConditionExpression: "TypeName = :typename AND EndTime > :timeleft",
+            ExpressionAttributeValues: {
+                ":typename": "Beacon",
+                ":timeleft": dateTimeNow
+            }
+        };
+
+        const data = await docClient.query(params).promise();
+        return data;
+    }
+}
